ebaa2504a4f0db97c4bcee9c17ae1f17d3271867
Saadullah Amjad <saad.dhamjad@gmail.com>
2025-09-07 14:54:17 +0500

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -27,6 +27,76 @@ export default function handler(req, res) {
         completionDate: "2025-07-20",
         certificateNumber: "MSN789",
         role: "Competitor"
+      },
+      "MSN101": {
+        name: "Fatima Zahra",
+        course: "Data Analyst",
+        completionDate: "2025-06-10",
+        certificateNumber: "MSN101",
+        role: "Student"
+      },
+      "MSN102": {
+        name: "Bilal Hussain",
+        course: "Python Programming",
+        completionDate: "2025-06-18",
+        certificateNumber: "MSN102",
+        role: "Intern"
+      },
+      "MSN103": {
+        name: "Ayesha Malik",
+        course: "Generative Artificial Intelligence (Gen AI)",
+        completionDate: "2025-05-25",
+        certificateNumber: "MSN103",
+        role: "Student"
+      },
+      "MSN104": {
+        name: "Usman Iqbal",
+        course: "Software Engineering with ASP.NET Core",
+        completionDate: "2025-05-12",
+        certificateNumber: "MSN104",
+        role: "Intern"
+      },
+      "MSN105": {
+        name: "Hira Nadeem",
+        course: "Full Stack Graphic Designing with UI UX",
+        completionDate: "2025-04-28",
+        certificateNumber: "MSN105",
+        role: "Student"
+      },
+      "MSN106": {
+        name: "Ahmad Raza",
+        course: "Digital Marketing with AI tools",
+        completionDate: "2025-04-05",
+        certificateNumber: "MSN106",
+        role: "Intern"
+      },
+      "MSN107": {
+        name: "Maryam Sheikh",
+        course: "Frontend Development",
+        completionDate: "2025-03-22",
+        certificateNumber: "MSN107",
+        role: "Student"
+      },
+      "MSN108": {
+        name: "Imran Khan",
+        course: "MERN Stack",
+        completionDate: "2025-03-08",
+        certificateNumber: "MSN108",
+        role: "Intern"
+      },
+      "MSN109": {
+        name: "Sana Fatima",
+        course: "Wordpress Development",
+        completionDate: "2025-02-18",
+        certificateNumber: "MSN109",
+        role: "Student"
+      },
+      "MSN110": {
+        name: "Danish Ali",
+        course: "Competitive Programming",
+        completionDate: "2025-01-30",
+        certificateNumber: "MSN110",
+        role: "Competitor"
       }
     };
 
